8a85f2ea366111c80f959eb7991f01c5e858b803
Nicolás Tamargo <reosarevok@gmail.com>
2016-04-22 12:55:21 +0300

    Adapt footer / header

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,10 @@
 <footer>
     <div class="row">
         <div class="col-xs-12 col-sm-6 text-left">
-            <p>Fahrenheit 451</p>
-            <p>Aparaaditehas</p>
-            <p>Kastani 42, Tartu</p>
+            <p>Nokkloomad on toredad</p>
         </div>
         <div class="col-xs-12 col-sm-6 text-right">
-            <p>Avatud K-P</p>
-            <p>12:00-18:00</p>
+            <p>Laiskloomad on toredad</p>
         </div>
     </div>
 </footer>
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header>
     <div class="row">
         <div class="col-xs-12">
-            <h1 class="text-center">Raamatupood Fahrenheit 451</h1>
+            <h1 class="text-center">Printing Museum Cover System</h1>
         </div>
     </div>
 </header>
