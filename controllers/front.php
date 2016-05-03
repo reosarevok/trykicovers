2f89ece996033f6df2e599af935fb1c58de34219
Nicolás Tamargo <reosarevok@gmail.com>
2016-05-03 16:39:40 +0300

    Show all covers on the front page

--- controllers/front.php
+++ controllers/front.php
@@ -3,6 +3,4 @@ require $_SERVER['DOCUMENT_ROOT']."/trykicovers/system/database.php";
 
     $covers = get_all("SELECT *
                    FROM cover
-                   JOIN cover_image USING (cover_id)
-                   ORDER BY cover_id DESC
-                   LIMIT 5");
+                   JOIN cover_image USING (cover_id)");
--- views/front.php
+++ views/front.php
@@ -3,22 +3,23 @@
 <?php if (count($covers) > 0): ?>
 <div class="row">
     <div class="col-xs-12">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Price</th>
-            </tr>
-            </thead>
+        <table class="table">
             <tbody>
-            <?php foreach ($covers as $cover): array_map('htmlentities', $cover); ?>
-                <tr>
-                    <td><?= $cover["title"] ?></td>
-                    <td><?= $cover["name"] ?></td>
-                    <td><?= $cover["price"] ?></td>
-                </tr>
-            <?php endforeach; ?>
+            <tr>
+            <?php
+
+            $count = 0;
+            foreach ($covers as $cover) {
+                echo "<td>";
+                display_cover($cover['cover_id']);
+                echo "</td>";
+                $count++;
+                if ($count >= 3) {
+                    echo "</tr><tr>";
+                    $count = 0;
+                }
+            } ?>
+            </tr>
             </tbody>
         </table>
     </div>
