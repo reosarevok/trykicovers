f6a295d81ed66ee2095da76fbffb42b2a3184d98
Nicolás Tamargo <reosarevok@gmail.com>
2016-05-03 16:21:50 +0300

    Adding a navbar

--- templates/header.php
+++ templates/header.php
@@ -3,5 +3,25 @@
         <div class="col-xs-12">
             <h1 class="text-center">Printing Museum Cover System</h1>
         </div>
+        <div class="col-xs-12">
+            <nav class="navbar navbar-default">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="admin.php">Admin</a></li>
+                        <li><a href="search.php">Search</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </div>
 </header>
