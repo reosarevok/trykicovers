060c515ba10bcb13b0a0bf8652d4f79a07fefb11
Nicolás Tamargo <reosarevok@gmail.com>
2017-05-26 22:58:31 +0300

    Properly link to minified jQuery

--- templates/master_template.php
+++ templates/master_template.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="vendor/bootstrap-tagsinput/bootstrap-tagsinput-css/bootstrap-tagsinput.css"/>
     <link href="static/style/style.css" rel="stylesheet" type="text/css">
-    <script src="vendor/jquery/jquery/jquery-3.1.1.js"></script>
+    <script src="vendor/jquery/jquery/jquery-3.1.1.min.js"></script>
     <script src="vendor/bootstrap-tagsinput/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
     <script src="vendor/typeahead.js/typeahead.js/typeahead.bundle.min.js"></script>
     <script src="vendor/transliteration.cyr/transliteration.cyr/transliteration.cyr.js"></script>
